1d13b747beb455601d02702351c3eaefa98cb09b
drdr xp <drdr.xp@gmail.com>
2015-01-06 01:33:26 +0800

    fix docindex

--- _script/docindex.py
+++ _script/docindex.py
@@ -3,40 +3,53 @@
 
 import os
 
-outfn = os.path.join( "doc", "xpt", "options.txt" )
-outf = open( outfn, "w" )
+def out( f, *msgs ):
+    for msg in msgs:
+        f.write(msg + "\n" )
 
-def make_option_index( indent="	"*2 ):
-    base = os.path.join( "doc", "xpt", "options" )
+def fnlist( section ):
+    base = os.path.join( "doc", "xpt", section )
     fns = os.listdir( base )
     fns.sort()
+    return fns
 
-    for fn in fns:
-        optname = fn.split(".")[0]
-        out( indent + "|g:xptemplate_" + optname + "|" )
+def fread( fn ):
+    with open( fn, 'r' ) as f:
+        cont = f.read()
+    lines = cont.split( "\n" )
+    lines = [ x for x in lines
+              if not x.startswith( '" vi''m:' ) ]
+    return '\n'.join( lines )
 
-def update_help_tags():
-    os.system( 'vim -c "helptags ./doc" -c "qa!"' )
+def merge_doc( section, prefix ):
 
-def out( *msgs ):
-    for msg in msgs:
-        outf.write(msg + "\n" )
+    header = [
+            '							 *xpt-' + section + '*',
+            '',
+    ]
+
+    fns = fnlist( section )
+
+    fn = os.path.join( "doc", "xpt", section + ".txt" )
+    with open( fn, "w" ) as f:
+
+        out( f, *header )
+
+        indent = "		"
+        for fn in fns:
+            optname = fn.split(".")[0]
+            out( f, indent + "|" + prefix + optname + "|" )
+
+        out( f, "" )
+
+        for fn in fns:
+            cont = fread( os.path.join( "doc", "xpt", section, fn ) )
+            out( f, cont )
+
+        out( f, "" )
+        out( f, '" vi''m: tw=78:ts=8:sw=8:sts=8:noet:ft=help:norl:' )
 
 if __name__ == "__main__":
 
-    option_header = [
-            '								 *xpt-option*',
-            '	Options:',
-    ]
-
-    for line in option_header:
-        out( line )
-
-    make_option_index()
-
-    out( "" )
-    out( '" vi''m: tw=78:ts=8:sw=8:sts=8:noet:ft=help:norl:' )
-
-    outf.close()
-
-    update_help_tags()
+    merge_doc( 'option', 'g:xptemplate_' )
+    merge_doc( 'snippet-function', '' )
