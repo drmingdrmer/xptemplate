b46775ca2d491301255e71af5a2a7eaa7bad817e
drdr xp <drdr.xp@gmail.com>
2015-11-27 00:13:53 +0800

    -b for bench

--- test/unittest.sh
+++ test/unittest.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+bench=0
+while :; do
+    case $1 in
+        -b)
+            bench=1
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
 pattern=${1-*}
+
 # unalias vim
 vim -u ./test/core_vimrc \
-    -c 'call xpt#unittest#Runall('"'$pattern'"') | if confirm("quit","&q\nn") == 1 | qa | endif'
+    -c 'call xpt#unittest#Runall('$bench", '$pattern'"') | if confirm("quit","&q\nn") == 1 | qa | endif'
